4e163be7fdc5126236102b7a49a4145ef7755520
Morris135212 <chenhui.m@columbia.edu>
2021-10-30 17:40:54 -0400

    Add templates

---
+++ app/models.py
@@ -0,0 +1,57 @@
+class Competition:
+    def __init__(self):
+        name = None
+        start_date = None
+        end_date = None
+        prize = None
+
+
+class DataSet:
+    def __init__(self):
+        idx = None
+        name = None
+        provenance = None
+
+
+class Users:
+    def __init__(self):
+        u_id = None
+        university_name = None
+        f_name = None
+        l_name = None
+        webpage_link = None
+        gold_medal = 0
+        silver_medal = 0
+        bronze_medal = 0
+
+
+class Codes:
+    def __init__(self):
+        idx = None
+        t_id = None
+        u_id = None
+        code = None
+        output_file = None
+        submit_date = None
+        stars = None
+
+
+class Tasks:
+    def __init__(self):
+        t_id = None
+        d_id = None
+        name = None
+        description = None
+
+
+class University:
+    def __init__(self):
+        name = None
+        location = None
+
+
+class Course:
+    def __int__(self):
+        idx = None
+        name = None
+        description = None
