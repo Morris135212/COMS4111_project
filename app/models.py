9fb1071ebd28ad853d815279a02353c037679078
Morris135212 <chenhui.m@columbia.edu>
2021-10-30 20:41:39 -0400

    update

--- app/models.py
+++ app/models.py
@@ -1,57 +1,71 @@
 class Competition:
-    def __init__(self):
-        name = None
-        start_date = None
-        end_date = None
-        prize = None
+    def __init__(self, name, start_date, end_date, prize):
+        self.name = name
+        self.start_date = str(start_date)
+        self.end_date = str(end_date)
+        self.prize = prize
+
+    def __str__(self):
+        return f"competition: {self.name}, " \
+               f"start from: {self.start_date}, " \
+               f"end at: {self.end_date} with prize: {self.prize}"
 
 
 class DataSet:
-    def __init__(self):
-        idx = None
-        name = None
-        provenance = None
+    def __init__(self, idx, name, provenance):
+        self.idx = idx
+        self.name = name
+        self.provenance = provenance
 
 
 class Users:
-    def __init__(self):
-        u_id = None
-        university_name = None
-        f_name = None
-        l_name = None
-        webpage_link = None
-        gold_medal = 0
-        silver_medal = 0
-        bronze_medal = 0
+    def __init__(self, u_id, university_name, f_name,
+                 l_name, webpage_link,
+                 gold_medal=0, silver_medal=0, bronze_medal=0):
+        self.u_id = u_id
+        self.university_name = university_name
+        self.f_name = f_name
+        self.l_name = l_name
+        self.webpage_link = webpage_link
+        self.gold_medal = gold_medal
+        self.silver_medal = silver_medal
+        self.bronze_medal = bronze_medal
+
+
+class Participants:
+    def __init__(self, f_name, l_name, score):
+        self.f_name = f_name
+        self.l_name = l_name
+        self.score = score
 
 
 class Codes:
-    def __init__(self):
-        idx = None
-        t_id = None
-        u_id = None
-        code = None
-        output_file = None
-        submit_date = None
-        stars = None
+    def __init__(self, idx, t_id, u_id, code, output_file, submit_date, stars):
+        self.idx = idx
+        self.t_id = t_id
+        self.u_id = u_id
+        self.code = code
+        self.output_file = output_file
+        self.submit_date = submit_date
+        self.stars = stars
 
 
 class Tasks:
-    def __init__(self):
-        t_id = None
-        d_id = None
-        name = None
-        description = None
+    def __init__(self, t_id, d_id, name, description):
+        self.t_id = t_id
+        self.d_id = d_id
+        self.name = name
+        self.description = description
 
 
 class University:
-    def __init__(self):
-        name = None
-        location = None
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
 
 
 class Course:
-    def __int__(self):
-        idx = None
-        name = None
-        description = None
+    def __int__(self, idx, name, description):
+        self.idx = idx
+        self.name = name
+        self.description = description
--- app/views.py
+++ app/views.py
@@ -1,7 +1,48 @@
 # -*- coding: utf-8 -*-
 from app import app
+from flask import Flask, request, render_template, g, redirect, Response
+
+from app.models import Competition, Participants
 
 
 @app.route('/')
 def index():
-    return 'hello world, hello flask'
+    return render_template("index.html")
+
+
+@app.route('/competition', methods=['GET', 'POST'])
+def comp():
+    name = request.form['comp']
+    print(f"The name of competition {name}")
+    cursor = g.conn.execute('SELECT * FROM competitions WHERE name = %s', name)
+    result = cursor.fetchone()
+    competition = Competition(*result)
+    # print(competition)
+    sql = "SELECT u.f_name, u.l_name, t.score from USERS u " \
+          "INNER JOIN  Take t ON u.u_id = t.u_id " \
+          "WHERE t.c_name = %s " \
+          "ORDER BY t.score DESC " \
+          "LIMIT 10"
+    cursor = g.conn.execute(sql, name).fetchall()
+
+    rank = []
+    f_name, l_name, scores = [], [], []
+    for i, result in enumerate(cursor):
+        participant = Participants(*result)
+        rank.append(i+1)
+        f_name.append(participant.f_name)
+        l_name.append(participant.l_name)
+        scores.append(participant.score)
+
+    context = dict(competition=competition.name,
+                   start_date=competition.start_date,
+                   end_date=competition.end_date,
+                   prize=competition.prize,
+                   rank=rank,
+                   f_name=f_name,
+                   l_name=l_name,
+                   scores=scores)
+    print(context)
+    return render_template("competition.html", **context)
+
+
