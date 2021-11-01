f6a631015ad00bb34a7ffee44b00187a2b7f7d9a
Morris135212 <chenhui.m@columbia.edu>
2021-10-31 22:26:17 -0400

    update

--- app/views.py
+++ app/views.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+import datetime
+
 from app import app
 from flask import Flask, request, render_template, g, redirect, Response
 
-from app.models import Competition, Participants
+from app.models import Competition, Participants, Users
+
+COMPETITIONS = ['Titanic - Machine Learning from Disaster', 'NFL Health & Safety - Helmet Assignment']
 
 
 @app.route('/')
@@ -52,13 +56,24 @@ def sign():
     lname = request.form['lname']
     web = request.form['web']
     university = request.form['university']
-    print(f"fname: {fname}, lname: {lname}, web:{web}, university: {university}")
+    competition = request.form['comp']
+    import re
+    competition = list(filter(lambda x: re.compile(competition).findall(x), COMPETITIONS))[0]
+    print(f"fname: {fname}, lname: {lname}, web:{web}, university: {university}, competition: {competition}")
     sql = "SELECT * FROM USERS " \
           "WHERE f_name = %s AND l_name = %s AND webpage_link = %s AND UNIVERSITY_name = %s"
     results = g.conn.execute(sql, fname, lname, web, university).fetchall()
-    print(results)
     if results:
-        return render_template("index.html")
+        user = Users(*results[0])
+        try:
+            insert_sql = "INSERT INTO take (score, paticipant_date, u_id, c_name) VALUES (%s, %s, %s, %s)"
+            import datetime
+            now_time = datetime.datetime.now().strftime('%Y-%m-%d')
+            g.conn.execute(insert_sql, .0, now_time, user.u_id, competition)
+        except Exception as e:
+            context = dict(error="You have joined the competition")
+            return render_template("500.html", **context)
+        return redirect('/')
     context = dict(error="User not found in database")
     return render_template("500.html", **context)
 
