61fa1a15816a0c463f78e929ae8648072fc07517
Morris135212 <chenhui.m@columbia.edu>
2021-11-04 15:04:55 -0400

    update

--- app/views/dataset.py
+++ app/views/dataset.py
@@ -1,7 +1,8 @@
-from app import app
-from flask import Flask, request, render_template, g, redirect, Response
+from flask import request, render_template, g, redirect
 
-from app.models import DataSets, Tasks
+from app import app
+from app.models import DataSets, Tasks, Users
+import numpy as np
 
 COMPETITIONS = ['Titanic - Machine Learning from Disaster', 'NFL Health & Safety - Helmet Assignment']
 
@@ -43,3 +44,42 @@ def detail():
 def contribute():
     context = dict(operation="Contribute")
     return render_template("contribute.html", **context)
+
+
+@app.route("/operation", methods=["POST", "GET"])
+def operation():
+    op = request.form["operation"]
+    fname = request.form["fname"]
+    lname = request.form["lname"]
+    dname = request.form["dname"]
+    provenance = request.form["provenance"]
+    print(f"fname:{fname}, lname:{lname}, dname:{dname}")
+    if op == "Contribute":
+        sql = "SELECT * FROM USERS " \
+              "WHERE f_name = %s AND l_name = %s"
+        results = g.conn.execute(sql, fname, lname).fetchall()
+        print(results)
+        try:
+            assert results, "You are not in database"
+            user = Users(*results[0])
+            try:
+                query_sql = "SELECT * FROM datasets WHERE name = %s AND provenance = %s"
+                results = g.conn.execute(query_sql, dname, provenance).fetchall()
+                assert not results, "You have contribute to this dataset before"
+
+                import uuid, datetime
+                idx = ''.join(np.random.choice(list(str(uuid.uuid1())), 20))
+                insert_sql1 = "INSERT INTO datasets (name, idx, provenance) VALUES (%s, %s, %s)"
+                g.conn.execute(insert_sql1, dname, idx, provenance)
+                now_time = datetime.datetime.now().strftime('%Y-%m-%d')
+                insert_sql2 = "INSERT INTO maintain (update_time, u_id, d_id) VALUES (%s, %s, %s)"
+                g.conn.execute(insert_sql2, now_time, user.u_id, idx)
+                context = dict(message="You have successfully add a new dataset!")
+            except Exception as e:
+                context = dict(error=e)
+                return render_template("500.html", **context)
+            return render_template('success.html', **context)
+        except Exception as e:
+            print(e)
+            context = dict(error=e)
+            return render_template("500.html", **context)
