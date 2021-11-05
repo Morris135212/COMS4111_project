3b2a0de2e3ef04b0fce5b4c1e8ce0ce3e7fc78c8
Morris135212 <chenhui.m@columbia.edu>
2021-11-05 11:02:02 -0400

    update follower template

--- app/views/users.py
+++ app/views/users.py
@@ -15,3 +15,20 @@ def users():
     context = dict(users=users, USER="USER")
     print(context)
     return render_template("users.html", **context)
+
+
+@app.route("/follower")
+def followers():
+    """
+    Search followers for certain users
+    //Who follows users – whose first name is Hiroshi and last name is Oshio?
+    SELECT u1.u_id, u1.f_name, u1.l_name FROM USERS u1
+    WHERE u1.u_id in (
+	    SELECT f.follower_id FROM USERS u2
+	    INNER JOIN
+	    FOLLOW f
+	    ON
+	    u2.u_id = f.followed_id
+	    WHERE u2.f_name = 'Hiroshi' AND u2.l_name = 'Oshio');
+    """
+    pass
