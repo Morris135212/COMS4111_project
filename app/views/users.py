1546e3e8f9a11f6a161b20e8e302fc09bc404bb9
KXVII <jxnchzh@qq.com>
2021-11-05 18:10:47 -0400

    增加了查询粉丝的功能，但是我不知道如何传递两个表到页面，因此可能需要另一个按钮点击查询关注

--- app/views/users.py
+++ app/views/users.py
@@ -17,18 +17,25 @@ def users():
     return render_template("users.html", **context)
 
 
-@app.route("/followers")
+@app.route("/followers", methods=["POST", "GET"])
 def followers():
-    """
-    Search followers for certain users
-    //Who follows users – whose first name is Hiroshi and last name is Oshio?
-    SELECT u1.u_id, u1.f_name, u1.l_name FROM USERS u1
-    WHERE u1.u_id in (
-	    SELECT f.follower_id FROM USERS u2
-	    INNER JOIN
-	    FOLLOW f
-	    ON
-	    u2.u_id = f.followed_id
-	    WHERE u2.f_name = 'Hiroshi' AND u2.l_name = 'Oshio');
-    """
-    pass
+    u_id = request.form["u_id"]
+    sql = "SELECT * FROM users WHERE u_id = %s"
+    user = g.conn.execute(sql, u_id).fetchall()[0]
+    user = Users(*user)
+    # following_query = "SELECT u.u_id, u.f_name, u.l_name, u.webpage_link, u.gold_medal, " \
+    #                   "u.silver_medal, u.bronze_medal, u.university_name" \
+    #                   "FROM follow f JOIN users u ON f.followed_id = u.u_id" \
+    #                   "WHERE f.follower_id = %s"
+    followed_query = "SELECT u.u_id, u.f_name, u.l_name, u.webpage_link, u.gold_medal, " \
+                     "u.silver_medal, u.bronze_medal, u.university_name " \
+                     "FROM follow f JOIN users u ON f.follower_id = u.u_id " \
+                     "WHERE f.followed_id = %s"
+    results = g.conn.execute(followed_query, u_id).fetchall()
+    followers = []
+    for i, result in enumerate(results):
+        follower = Users(*result)
+        followers.append(follower.__dict__)
+    context = dict(users=followers, USER="Followers of "+user.f_name+' '+user.l_name)
+    print(f"{context}, 页面主人: {u_id}")
+    return render_template("users.html", **context)
